76680bbf41d75c43b7130f4fdc892c9fb85ca0eb
xma24 <xma24@buffalo.edu>
2023-12-30 19:52:28 -0600

    update setup

--- setup.py
+++ setup.py
@@ -1,15 +1,44 @@
-from setuptools import setup, find_packages
+# from setuptools import setup, find_packages
 
-setup(
+# setup(
+#     name="image_classification",
+#     version="0.2",
+#     license="MIT",
+#     description="Classifier for images",
+#     author="Xin Ma",
+#     author_email="xmafellowacm@gmail.com",
+#     packages=find_packages(),
+#     install_requires=[
+#         "pytorch_lightning",
+#         "dill",
+#         "torchvision",
+#         "timm",
+#         "Pillow",
+#         "numpy",
+#     ],
+# )
+
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import setuptools
+
+_VERSION = "0.1"
+
+REQUIRED_PACKAGES = []
+
+DEPENDENCY_LINKS = []
+
+setuptools.setup(
     name="image_classification",
-    version="0.2",
-    packages=find_packages(),
-    install_requires=[
-        "pytorch_lightning",
-        "dill",
-        "torchvision",
-        "timm",
-        "Pillow",
-        "numpy",
-    ],
+    version=_VERSION,
+    description="Classifier for images",
+    install_requires=REQUIRED_PACKAGES,
+    dependency_links=DEPENDENCY_LINKS,
+    url="https://github.com/xma24/image_classification",
+    license="MIT License",
+    package_dir={},
+    packages=setuptools.find_packages(exclude=["tests"]),
 )
