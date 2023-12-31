f1510ec69d110c458ebdc11f3abb9637acdb85db
xma24 <xma24@buffalo.edu>
2023-12-30 19:46:03 -0600

    update folder structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ImageClassification",
+    name="image_classification",
     version="0.2",
     packages=find_packages(),
     install_requires=[
