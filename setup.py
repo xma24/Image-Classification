20d737c564cdac10a260327055993ffbc0c48a4b
xma24 <xma24@buffalo.edu>
2023-12-30 19:21:12 -0600

    update setup to install package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="image-classification",
+    name="image_classification",
     version="0.1",
     packages=find_packages(),
     install_requires=[
